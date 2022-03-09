cd5dffbfed27c566d4fd42cbb867263be35e62e5
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2022-03-09 14:21:37 +0000

    Update progress

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -2,7 +2,7 @@ import classes from './ProgressBar.module.css'
 
 const ProgressBar = () => {
     const goal = 2000;
-    const current = 39.72;
+    const current = 39.72 + 160;
 
     const ratio = current / goal;
 
