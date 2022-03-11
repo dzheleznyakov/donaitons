8afee9b18ec6aa18cc5da8afe44e033c63d75f5b
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2022-03-11 15:48:18 +0000

    Update text and details

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ProgressBar from './components/ProgressBar';
 import flag from './photos/flag.jpeg';
 
 function App() {
+  const current = 3128.47;
   return (
     <div className="App">
       <h1 style={{
@@ -22,25 +23,38 @@ function App() {
       <div className="TextWrapper">
         <ul>
           <li>An orphan house in Kharkov got severely damaged when a military operation began in the city. </li>
-          <li>Sadly a few children died</li>
-          <li>Volunteers managed to evacuate majority of children. </li>
-          <li>They have 29 children ranging from newborns to 7 year olds. </li>
-          <li>They are safe now and are looked after. </li>
-          <li>Only 5 volunteers are working on the site and taking shifts to maintain the place. </li>
-          <li>They do not receive any support or income.</li>
-          <li>They are getting some humanitarian help, and local people have no money to help them financially.</li>
+          <li>Volunteers managed to evacuate the majority of children.</li>
+          <li>We have about 125 children ranging from newborns to 10 year-olds. These children have recently become orphans and are emotionally traumatised.</li>
+          <li>55 volunteers are maintaining the place. They do not take days off and they do not receive any support or income.</li>
+          <li>They do not receive any support or income and the local people have no money to help them either. </li>
         </ul>
 
         <p style={{fontSize: '24px', fontWeight: 'bold', color: 'darkgreen'}}>
-          We are raising money to help maintain this place for the following six months.
+        We are raising money to respond to their urgent needs and help maintain these four places for the following six months.
         </p>
 
         <p style={{fontSize: '24px', color: 'rgb(26, 115, 232)'}}><b>
-          If you would like to help, the PayPal account is: <em>zheleznyakov.dmitry@gmail.com</em>
+          If you would like to help, the PayPal account is:{' '}
+          <em style={{textDecorationLine: 'underline'}}>zheleznyakov.dmitry@gmail.com</em>
         </b></p>
       </div>
 
-      <ProgressBar />
+      {/* <ProgressBar current={current} /> */}
+
+      <div className="TextWrapper">
+        <p>So far we've collected <b>£{current}</b> and with this money, we helped with:</p>
+        <ul className="Results">
+          <li>bought vital medicine for both the "Cafe" and the "School no. 3" that will last them for six months: on the way from Germany in 5 days (unavailable in Ukraine)</li>
+          <li>got two second-hand baby cots: to be delivered in 2 days (now little children can sleep safely and comfortably (as they slept on the foldable camping beds)</li>
+          <li>covered food expenses for the 5 volunteers for a month, so that they can continue taking care of the children without worrying about what they will eat</li>
+          <li>bought some fresh groceries (milk, bread, fruit)</li>
+          <li>got 5 warm jackets and 5 pairs of boots. 2 girls had their jackets torn. Now they are dressed warmly. The weather in Dnipro is -7C</li>
+          <li>bought pacifiers, muslin cloths, baby dry formula packs for the "School no. 3"</li>
+          <li>bought 2 second-hand prams: one for the "Cafe" and another on for the "School no. 3". They should arrive soon</li>
+          <li>and with what was left afterward helped out financially to the "School no. 3" (children shampoos, baby nappy creams, miscellanies) </li>
+</ul>
+      </div>
+
       <ImageCarousel />
 
 
--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,12 +1,15 @@
 import classes from './ProgressBar.module.css'
 
-const ProgressBar = () => {
+const ProgressBar = ({current}) => {
     const goal = 2000;
-    const current = 39.72 + 160 + 219.13 + 91.31 
-        + 14.6 + 16 + 200 + 100 + 100 + 73.05 + 21.91 + 218.39 + 363.98 + 100
-        + 145.59 + 218.39 + 72.79;
+    // const current = 39.72 + 160 + 219.13 + 91.31 
+        // + 14.6 + 16 + 200 + 100 + 100 + 73.05 + 21.91 + 218.39 + 363.98 + 100
+        // + 145.59 + 218.39 + 72.79;
+        // 2,910.09
+        // 3,346.86
+    // const current = 3128.47;
 
-    const ratio = current / goal;
+    // const ratio = current / goal;
 
     return (
         <div>
